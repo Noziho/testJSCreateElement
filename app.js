36be464349e86d43392d79cc6071a7a3117b9048
Noah2211 <noah.decroix3@gmail.com>
2021-11-17 13:29:18 +0100

    Second test

--- app.js
+++ app.js
@@ -5,3 +5,18 @@ newDiv.style.height = "200px";
 newDiv.style.background = "black";
 
 document.body.append(newDiv);
+
+function newElement(type) {
+    let newThings = document.createElement(type);
+    newThings.style.width = "200px";
+    newThings.style.height = "200px";
+    newThings.style.background = "black";
+    newThings.style.border = "3px solid red";
+    newThings.style.color = "white";
+    newThings.innerHTML = "test"
+    return newThings;
+}
+
+document.body.append(newElement("p"))
+document.body.append(newElement("span"))
+document.body.append(newElement("div"))
