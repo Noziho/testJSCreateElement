0dbf3b2b3fc1b689cc7bfdca24e7e5e10908c7b9
Noah2211 <noah.decroix3@gmail.com>
2021-11-17 13:20:06 +0100

    First test done

---
+++ app.js
@@ -0,0 +1,7 @@
+let newDiv = document.createElement("div");
+newDiv.style.border = "solid 1px black";
+newDiv.style.width = "200px";
+newDiv.style.height = "200px";
+newDiv.style.background = "black";
+
+document.body.append(newDiv);
